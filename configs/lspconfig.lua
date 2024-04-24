46fc971c42ae3e57b95eabf0dc7ef6ff7f7db83e
stevenmaharaj <stevesamah@gmail.com>
2024-04-24 19:07:32 +1000

    sql

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
 local servers =
-  { "html", "cssls", "tsserver", "gopls", "bufls", "rust_analyzer", "pyright", "clangd", "omnisharp", "solc" }
+  { "html", "cssls", "tsserver", "gopls", "bufls", "rust_analyzer", "pyright", "clangd", "omnisharp", "solc", "sqlls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
